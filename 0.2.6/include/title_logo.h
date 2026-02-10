f95b3c17e24d93e20a39a8126f5a9b4a199def0b
ValoTypeDark <valotypedark@gmail.com>
2026-02-10 14:16:39 +0000

    1

--- 0.2.6/include/title_logo.h
+++
@@ -1,9 +0,0 @@
-#ifndef TITLE_LOGO_H
-#define TITLE_LOGO_H
-
-#define TITLE_LOGO_WIDTH 220
-#define TITLE_LOGO_HEIGHT 132
-
-extern const unsigned short title_logoBitmap[29040];
-
-#endif // TITLE_LOGO_H
